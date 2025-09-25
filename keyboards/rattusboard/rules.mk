3049692541de3a78e242a250460becf85e0e6389
Mirda <72575324+Rattus-ukrizovany@users.noreply.github.com>
2025-09-25 15:58:15 +0200

    Update rules.mk

--- keyboards/rattusboard/rules.mk
+++ keyboards/rattusboard/rules.mk
@@ -10,6 +10,7 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+MOUSEKEY_ENABLE = yes
 
 # PMW3360 trackball support - configuration moved to keyboard.json
 POINTING_DEVICE_DRIVER = pmw3360
