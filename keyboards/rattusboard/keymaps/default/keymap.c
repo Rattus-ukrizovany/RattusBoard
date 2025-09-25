67947347aaa3e0230c68436226683fa4e25d7406
Mirda <72575324+Rattus-ukrizovany@users.noreply.github.com>
2025-09-25 15:57:36 +0200

    Refactor keymap layout and add mouse controls
    Updated the layout and structure of the keymap for better organization and functionality.

--- keyboards/rattusboard/keymaps/default/keymap.c
+++ keyboards/rattusboard/keymaps/default/keymap.c
@@ -36,7 +36,7 @@ enum custom_keycodes {
  * │LSFT │  A  │  S  │  D  │  F  │  G  │   │  H  │  J  │  K  │  L  │  ;  │  '  │
  * ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
  * │LCTL │  Z  │  X  │  C  │  V  │  B  │   │  N  │  M  │  ,  │  .  │  /  │ ESC │
- * └─────┴─────┴─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┴─────┴─────┘
+ * └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
  *                   │ GUI │SYMB │ SPC │   │LT(3,│ARWS │ ALT │
  *                   └─────┴─────┴─────┘   │ENT) │     │     │
  *                                         └─────┴─────┴─────┘
@@ -48,75 +48,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Base Layer (DEFAULT_L from ZMK)
      */
     [_DEFAULT] = LAYOUT_split_3x6_3(
-    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
-    //└────────┴────────┴────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┴────────┴────────┘
                                   KC_LGUI, SYMBOLS, KC_SPC,                    LT(_NUMBERS, KC_ENT), ARROWS,  KC_RALT
-    //                           └────────┴────────┴────────┘                          └────────┴────────┴────────┘
     ),
 
     /*
      * Symbols Layer (SYMBOLS_L from ZMK - was lower_layer)
      */
     [_SYMBOLS] = LAYOUT_split_3x6_3(
-    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_NO,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        QK_BOOT, QK_BOOT, QK_BOOT, KC_CAPS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS, QK_BOOT,
-    //└────────┴────────┴────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┴────────┴────────┘
                                   KC_LGUI, KC_TRNS, KC_SPC,                             KC_ENT,  KC_TRNS, KC_RALT
-    //                           └────────┴────────┴────────┘                          └────────┴────────┴────────┘
     ),
 
     /*
      * Arrows Layer (ARROWS_L from ZMK - was raise_layer)  
      */
     [_ARROWS] = LAYOUT_split_3x6_3(
-    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LCTL, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, QK_BOOT, QK_BOOT,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_NO,   KC_MINS, KC_VOLD, KC_VOLU, KC_PIPE, KC_TILD,
-    //└────────┴────────┴────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┴────────┴────────┘
                                   KC_LGUI, KC_TRNS, KC_SPC,                             KC_ENT,  KC_TRNS, KC_RALT
-    //                           └────────┴────────┴────────┘                          └────────┴────────┴────────┘
     ),
 
     /*
      * Numbers Layer (NUMBERS_L from ZMK - accessed via LT on Enter)
      */
     [_NUMBERS] = LAYOUT_split_3x6_3(
-    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    //└────────┴────────┴────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┴────────┴────────┘
                                   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS
-    //                           └────────┴────────┴────────┘                          └────────┴────────┴────────┘
     ),
 
     /*
      * Adjust Layer (Flash from ZMK - system controls, trackball, mouse)
      */
     [_ADJUST] = LAYOUT_split_3x6_3(
-    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
-    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX,
-    //└────────┴────────┴────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┴────────┴────────┘
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, XXXXXXX, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, XXXXXXX, XXXXXXX, XXXXXXX,
                                   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS
-    //                           └────────┴────────┴────────┘                          └────────┴────────┴────────┘
     )
 };
 
